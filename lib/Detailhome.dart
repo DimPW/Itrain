c466dd6aee275d631b576b10a68e740e239adbdc
Ario <@ariogalang7@gmail.com>
2022-11-28 07:41:49 +0700

    screen: [AG] screen home detail

--- lib/Detailhome.dart
+++ lib/Detailhome.dart
@@ -2,144 +2,166 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ticket_widget/ticket_widget.dart';
+import 'package:carousel_images/carousel_images.dart';
 
- class Homepage extends StatefulWidget {
-   const Homepage({Key? key}) : super(key: key);
- 
-   @override
-   State<Homepage> createState() => _HomepageState();
- }
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
 
- class _HomepageState extends State<Homepage> {
-   final List<String> imgList = [
-     'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-     'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-     'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-     'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-     'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-     'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
-   ];
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
 
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
-       backgroundColor: Colors.white,
-       appBar: AppBar(
-         elevation: 0,
-          backgroundColor: Colors.white,
-         title: const Text('Itrain',
-           style: TextStyle(fontWeight: FontWeight.bold, fontSize: (20), color: Colors.black),),
-         toolbarHeight: 110,
+class _HomepageState extends State<Homepage> {
+  final List<String> listImages = [
+    'assets/image/Itrain.png',
+    'assets/image/penggunabaru.png',
+    'assets/image/promo.png',
+    'assets/image/quote1.png',
+    'assets/image/quotedua.png'
+  ];
+  final List<String> imgList = [
+    'assets/image/Itrain.png',
+    'assets/image/penggunabaru.png',
+    'assets/image/promo.png',
+    'assets/image/quote1.png',
+    'assets/image/quotedua.png'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: const Text('Itrain',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: (20), color: Colors.black),),
+        toolbarHeight: 110,
 
 
-         flexibleSpace: Container(
-           margin: EdgeInsets.only(top: 83, left: 1, right: 150),
-           child: Image.asset(
-             "assets/black.png",
-             height: 13,
-             width: 13,
+        flexibleSpace: Container(
+          margin: EdgeInsets.only(top: 83, left: 1, right: 150),
+          child: Image.asset(
+            "assets/image/black.png",
+            height: 13,
+            width: 13,
 
-           ),
-         ),
-       ),
+          ),
+        ),
+      ),
 
-       body: SingleChildScrollView(
+      body: SingleChildScrollView(
 
-         child: Column(
+          child: Column(
 
-          children:[
-            SizedBox(height: 1,),
-            Padding(
-              padding: EdgeInsets.only(right:80, left: 15), //apply padding to some sides only
-              child:
-              Text(
-                'Where do you want to go?', maxLines: 2,
-                style: GoogleFonts.montserrat(
-                    fontWeight: FontWeight.bold, fontSize: 30
+              children:[
+                SizedBox(height: 1,),
+                Padding(
+                  padding: EdgeInsets.only(right:80, left: 15), //apply padding to some sides only
+                  child:
+                  Text(
+                    'Where do you want to go?', maxLines: 2,
+                    style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.bold, fontSize: 30
+                    ),
+                  ),
+
                 ),
-              ),
-
-            ),
-              SizedBox(height: 50,),
-            Padding(
-              padding: EdgeInsets.only(right:170, left: 20), //apply padding to some sides only
-              child:
-           Text(
-               'Your trip start from here!', maxLines: 2,
-               style: GoogleFonts.montserrat(
-                 fontWeight: FontWeight.bold, fontSize: 15
-               ),
-            ),
-            ),
-
-         SizedBox(height: 20,),
-         CarouselSlider(
-           options: CarouselOptions(
-             height: 220,
-             autoPlay: true,
-             aspectRatio: 2.0,
-             enlargeCenterPage: true,
-           ),
-           items: imgList
-               .map((item) => Container(
-             child: Container(
-               margin: EdgeInsets.all(1),
-               padding: EdgeInsets.only(top: 20),
-               child: ClipRRect(
-                   borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                   child: Stack(
-                     children: <Widget>[
-                       Image.network(item, fit: BoxFit.cover, width: 800),
-                       Positioned(
-                         child: Container(
-                           decoration: BoxDecoration(
-                             gradient: LinearGradient(
-                               colors: [
-                                 Color.fromARGB(200, 0, 0, 0),
-                                 Color.fromARGB(0, 0, 0, 0)
-                               ],
-                               begin: Alignment.bottomCenter,
-                               end: Alignment.topCenter,
-                             ),
-                           ),
-                           padding: EdgeInsets.symmetric(
-                               vertical: 20, horizontal: 20.0),
-                         ),
-                       ),
-                     ],
-                   )),
-             ),
-           ))
-               .toList(),
-         ),
-            SizedBox(height: 50,),
-            Padding(
-              padding: EdgeInsets.only(right:170, left: 20), //apply padding to some sides only
-              child:
-              Text(
-                'Your ongoing ticket', maxLines: 2,
-                style: GoogleFonts.montserrat(
-                    fontWeight: FontWeight.bold, fontSize: 15
+                SizedBox(height: 50,),
+                Padding(
+                  padding: EdgeInsets.only(right:170, left: 20), //apply padding to some sides only
+                  child:
+                  Text(
+                    'Your trip start from here!', maxLines: 2,
+                    style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.bold, fontSize: 15
+                    ),
+                  ),
                 ),
-              ),
-            ),
 
-            SizedBox(height: 50,),
+                SizedBox(height: 20,),
+                CarouselSlider(
+                  options: CarouselOptions(
+                    height: 220,
+                    autoPlay: true,
+                    aspectRatio: 2.0,
+                    enlargeCenterPage: true,
+                  ),
+                  items: imgList
+                      .map((item) => Container(
+                    child: Container(
+                      margin: EdgeInsets.all(1),
+                      padding: EdgeInsets.only(top: 20),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          child: Stack(
+                            children: <Widget>[
+                              Image.network(item, fit: BoxFit.cover, width: 800),
+                              Positioned(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                      colors: [
+                                        Color.fromARGB(0, 0, 0, 0),
+                                        Color.fromARGB(0, 0, 0, 0)
+                                      ],
+                                      begin: Alignment.bottomCenter,
+                                      end: Alignment.topCenter,
+                                    ),
+                                  ),
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 20, horizontal: 20.0),
+                                ),
+                              ),
+                            ],
+                          )),
+                    ),
+                  ))
+                      .toList(),
+                ),
+                SizedBox(height: 50,),
+                Padding(
+                  padding: EdgeInsets.only(right:170, left: 20), //apply padding to some sides only
+                  child:
+                  Text(
+                    'Your ongoing ticket', maxLines: 2,
+                    style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.bold, fontSize: 15
+                    ),
+                  ),
+                ),
 
-            TicketWidget(
-              color: Colors.grey,
-              width: 350,
-              height: 200,
-              isCornerRounded: true,
-              padding: EdgeInsets.all(20),
-              child: TicketData(),
-            ),
-          ]
-         )
-       ),
-       );
-   }
- }
+                SizedBox(height: 50,),
+
+                TicketWidget(
+                  color: Colors.grey,
+                  width: 350,
+                  height: 200,
+                  isCornerRounded: true,
+                  padding: EdgeInsets.all(20),
+                  child: TicketData(),
+                ),
+
+                SizedBox(height: 50,),
+
+                CarouselImages(
+                  scaleFactor: 0.6,
+                  listImages: listImages,
+                  height: 200,
+                  borderRadius: 30.0,
+                  cachedNetworkImage: true,
+                  verticalAlignment: Alignment.topCenter,
+                  onTap: (index){
+                    print('Tapped on page $index');
+                  },
+                )
+
+              ]
+          )
+      ),
+    );
+  }
+}
 
 class TicketData extends StatelessWidget{
   const TicketData({
@@ -154,11 +176,8 @@ class TicketData extends StatelessWidget{
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-
             Row(
               children: const [
-
-
                 Padding(
                   padding: EdgeInsets.only(left: 8.0),
                 )
@@ -166,6 +185,7 @@ class TicketData extends StatelessWidget{
             )
           ],
         ),
+
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -176,6 +196,7 @@ class TicketData extends StatelessWidget{
                   fontSize: 15,
                   fontWeight: FontWeight.bold),
             ),
+
             Padding(
               padding: const EdgeInsets.only(left: 47),
               child: Text(
@@ -186,6 +207,7 @@ class TicketData extends StatelessWidget{
                     fontWeight: FontWeight.bold),
               ),
             ),
+
             Padding(
               padding: const EdgeInsets.only(left: 47),
               child: Text(
@@ -198,6 +220,7 @@ class TicketData extends StatelessWidget{
             ),
           ],
         ),
+
         SizedBox(height: 15,),
         Padding(
           padding: const EdgeInsets.only(top: 5),
@@ -205,10 +228,8 @@ class TicketData extends StatelessWidget{
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
-
                 padding: const EdgeInsets.only(top: 5, right: 0),
                 child: ticketDetailsWidget('TMG', '11:30', 'KDS', '02:35'),
-
               ),
               SizedBox(height: 20,),
               Row(
@@ -223,10 +244,10 @@ class TicketData extends StatelessWidget{
                   ),
                 ],
               ),
-
             ],
           ),
         ),
+
         Padding(
           padding: const EdgeInsets.only(top: 0),
           child: Container(
@@ -240,9 +261,11 @@ class TicketData extends StatelessWidget{
 
 Widget ticketDetailsWidget(String firstTitle, String firstDesc,
     String secondTitle, String secondDesc) {
+
   return Row(
     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
     children: [
+
       Padding(
         padding: const EdgeInsets.only(left: 0),
         child: Column(
@@ -262,18 +285,16 @@ Widget ticketDetailsWidget(String firstTitle, String firstDesc,
           ],
         ),
       ),
+
       Icon(Icons.linear_scale_outlined,
         color: Colors.white,
         size: 25,),
       Icon(Icons.train_outlined,
         color: Colors.white,
-      size: 50,),
+        size: 50,),
       Icon(Icons.linear_scale_outlined,
         color: Colors.white,
         size: 25,),
-
-
-
 
       Padding(
         padding: const EdgeInsets.only(left: 0),
@@ -284,8 +305,8 @@ Widget ticketDetailsWidget(String firstTitle, String firstDesc,
             Text(
               secondTitle,
               style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-
             ),
+
             Padding(
               padding: const EdgeInsets.only(top: 4.0),
 
@@ -299,8 +320,6 @@ Widget ticketDetailsWidget(String firstTitle, String firstDesc,
       ),
     ],
   );
-
-
 }
 
 
